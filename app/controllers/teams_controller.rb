3ca525ca8749a53dd7b7c80a66d544b341230a06
reiko enomoto <ooreicookoo@gmail.com>
2021-01-10 16:04:58 +0900

    eamのオーナーか、そのUser自身しかできないようにすること TeamのeditはTeamのリーダー（オーナー）のみができるようにすること

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,7 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_team, only: %i[show edit update destroy]
+  before_action :not_authorized, only: %i[edit destroy]
 
   def index
     @teams = Team.all
@@ -55,5 +56,11 @@ class TeamsController < ApplicationController
 
   def team_params
     params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
+  end
+
+  def not_authorized
+    if current_user.id != @team.owner_id
+      redirect_to root_path
+  end
   end
 end
